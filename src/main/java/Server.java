5bd3371d29ea68e2f162b1c35cc158c312c4a994
Anton Khlebnikov <toni.farsh@icloud.com>
2022-01-24 12:12:37 +0500

    Add HW

---
+++ src/main/java/Server.java
@@ -0,0 +1,30 @@
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class Server {
+    private final int port;
+
+    public Server(int port) {
+        this.port = port;
+    }
+
+    public void listen() {
+        final ExecutorService threadPool = Executors.newFixedThreadPool(64);
+        try (final var serverSocket = new ServerSocket(port)) {
+            while (true) {
+                try {
+                    Socket socket = serverSocket.accept();
+                    threadPool.execute(new Handler(socket));
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
